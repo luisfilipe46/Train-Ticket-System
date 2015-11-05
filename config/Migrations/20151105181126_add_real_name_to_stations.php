95254af6c96fc837c6cf781741abca47af3a61bc
Luis Araujo <ei11059@fe.up.pt>
2015-11-05 18:19:06 +0000

    a

---
+++ config/Migrations/20151105181126_add_real_name_to_stations.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddRealNameToStations extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('stations');
+        $table->addColumn('real_name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
