c4cfa313ce21aa16451357d78534feb35bb8fa5d
Luis Araujo <ei11059@fe.up.pt>
2015-11-05 18:19:42 +0000

    a

--- config/seed.php
+++ config/seed.php
@@ -47,41 +47,6 @@ $data = [
 		'_truncate' => true,
 		'_defaults' => ['created' => '2016-06-06 01:00:00',
 			'modified' => '2016-06-06 01:00:00'],
-		[
-			'name' => '11', // 1 = A
-			'line' => 'A',
-			'real_name' => 'S. Joao'
-		],
-		[
-			'name' => '12', // 1 = A
-			'line' => 'A',
-			'real_name' => 'IPO'
-		],
-		[
-			'name' => '01', // 0 = A, B, C
-			'line' => '0',
-			'real_name' => 'Trindade'
-		],
-		[
-			'name' => '21', // 2 = B
-			'line' => 'B',
-			'real_name' => 'Aliados'
-		],
-		[
-			'name' => '22', // 2 = B
-			'line' => 'B',
-			'real_name' => 'Faria Guimaraes',
-		],
-		[
-			'name' => '31', // 3 = C
-			'line' => 'C',
-			'real_name' => 'Azurara',
-		],
-		[
-			'name' => '32', // 3 = C
-			'line' => 'C',
-			'real_name' => 'Vila do Conde',
-		],
 	],
 ];
 
